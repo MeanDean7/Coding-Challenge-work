bf85759556ead221e08c959e1bcb53226932ec31
MeanDean7 <35509928+MeanDean7@users.noreply.github.com>
2018-03-16 09:59:16 -0400

    Add files via upload

---
+++ game/src/TicTacToe.java
@@ -0,0 +1,119 @@
+import java.util.Arrays;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import java.awt.Color;
+import java.awt.Font;
+
+public class TicTacToe {
+	static Scanner in;
+	static String[] board;
+	static String turn;
+
+	public static void main(String[] args) {
+		in = new Scanner(System.in);
+		board = new String[9];
+		turn = "X";
+		String winner = null;
+		populateEmptyBoard();
+
+		System.out.println("Welcome to 2 Player Tic Tac Toe.");
+		System.out.println("--------------------------------");
+		printBoard();
+		System.out.println("X's will play first. Enter a spot number to place X in:");
+
+		while (winner == null) {//Checks if the value is acceptable
+			int numInput;
+			try {
+				numInput = in.nextInt();
+				if (!(numInput > 0 && numInput <= 9)) {
+					System.out.println("Invalid input; re-enter spot number:");
+					continue;
+				}
+			} catch (InputMismatchException e) {
+				System.out.println("Invalid input; re-enter spot number:");
+				continue;
+			}
+			if (board[numInput-1].equals(String.valueOf(numInput))) {
+				board[numInput-1] = turn;
+				if (turn.equals("X")) {
+					turn = "O";
+				} else {
+					turn = "X";
+				}
+				printBoard();
+				winner = checkWinner();
+			} else {
+				System.out.println("Spot already taken; re-enter number of space that is not taken:");
+				continue;
+			}
+		}
+		if (winner.equalsIgnoreCase("draw")) {//conditions for a tie
+			System.out.println("It's a draw! Thanks for playing.");
+		} else {//conditions for winning
+			System.out.println("Congratulations! " + winner + "'s have won! Thanks for playing Tic-Tac-Toe!");
+		}
+	}
+
+	static String checkWinner() {//determines who is the winner
+		for (int a = 0; a < 8; a++) {
+			String line = null;
+			switch (a) {
+			case 0:
+				line = board[0] + board[1] + board[2];
+				break;
+			case 1:
+				line = board[3] + board[4] + board[5];
+				break;
+			case 2:
+				line = board[6] + board[7] + board[8];
+				break;
+			case 3:
+				line = board[0] + board[3] + board[6];
+				break;
+			case 4:
+				line = board[1] + board[4] + board[7];
+				break;
+			case 5:
+				line = board[2] + board[5] + board[8];
+				break;
+			case 6:
+				line = board[0] + board[4] + board[8];
+				break;
+			case 7:
+				line = board[2] + board[4] + board[6];
+				break;
+			}
+			if (line.equals("XXX")) {
+				return "X";
+			} else if (line.equals("OOO")) {
+				return "O";
+			}
+		}
+
+		for (int a = 0; a < 9; a++) {
+			if (Arrays.asList(board).contains(String.valueOf(a+1))) {
+				break;
+			}
+			else if (a == 8) return "draw";
+		}
+		//determines whose turn it is
+		System.out.println(turn + "'s turn; enter a number to place your marker " + turn + " in:");
+		return null;
+	}
+
+	static void printBoard() {//Prints the actual game board
+		System.out.println("/---|---|---\\");
+		System.out.println("| " + board[0] + " | " + board[1] + " | " + board[2] + " |");
+		System.out.println("|-----------|");
+		System.out.println("| " + board[3] + " | " + board[4] + " | " + board[5] + " |");
+		System.out.println("|-----------|");
+		System.out.println("| " + board[6] + " | " + board[7] + " | " + board[8] + " |");
+		System.out.println("/---|---|---\\");
+	}
+
+	static void populateEmptyBoard() {//Restarts the board
+		for (int a = 0; a < 9; a++) {
+			board[a] = String.valueOf(a+1);
+		}
+	}
+}
